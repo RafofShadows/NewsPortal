9cb0a9b750180ffd042ee963e66214644a609419
RafofShadows <pickers97@yandex.ru>
2022-11-14 18:38:41 +0300

    Задание по модулю D5

---
+++ news/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Author(models.Model):
+    rating = models.IntegerField(default=0)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def update_rating(self):
+        res = 0
+        for post in Post.objects.filter(author=self.pk):
+            res += post.rating * 3
+            for comment in Comment.objects.filter(post=post.pk):
+                res += comment.rating
+        for comment in Comment.objects.filter(user_id=self.user_id):
+            res += comment.rating
+        self.rating = res
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+News = 'NEW'
+Article = 'ART'
+TYPES = [
+    (News, 'Новость'),
+    (Article, 'Статья')
+]
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_type = models.CharField(max_length=3, choices=TYPES, default='ART')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    header = models.CharField(max_length=255, default='')
+    text = models.TextField(default='')
+    rating = models.IntegerField(default=0)
+
+    categories = models.ManyToManyField(Category, through='PostCategory')
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[:124] + '...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(default='')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
